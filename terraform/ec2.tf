5ad55f253886de6ac7335f39148374aa5ad9bbec
Gabriel Natan <gabriel--natan@live.com>
2024-12-16 00:24:44 -0300

    changes

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,11 @@ resource "aws_instance" "amb-prod-2" {
   ami             = "ami-0e2c8caa4b6378d8c"
   instance_type   = "t2.micro"
   security_groups = ["allow_http", "allow_ssh", "allow_egress"]
-  user_data       = file("script.sh")
+
+  # Interpolação do script com o valor do PAT_TOKEN
+  user_data = templatefile("${path.module}/script.sh", {
+    PAT_TOKEN = var.github_secret
+  })
 
   tags = {
     Name = "amb-prod"
--- terraform/script.sh
+++ terraform/script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo "Iniciando a configuração"
-    
+
+# Atualiza o sistema e instala pacotes necessários
 apt-get update
 apt-get install -y curl
+sudo apt install docker.io -y
 
-sudo apt install docker.io  -y
+# Faz login no Docker usando o token interpolado
+echo "${PAT_TOKEN}" | docker login ghcr.io -u user --password-stdin
 
-echo "${var.github_secret}" | docker login ghcr.io -u gabrielnatan --password-stdin
-docker pull ghcr.io/gabrielnatan/front:main
-docker run -d -p 80:80 ghcr.io/gabrielnatan/front:main
+# Baixa a imagem do contêiner e executa
+docker pull ghcr.io/user/front:main
+docker run -d -p 80:80 ghcr.io/user/front:main
