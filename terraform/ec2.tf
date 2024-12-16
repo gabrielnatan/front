6cf79920cb010024ab388aec01cb9e1280c5c3df
Gabriel Natan <gabriel--natan@live.com>
2024-12-16 00:39:58 -0300

    voltando ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,11 +2,7 @@ resource "aws_instance" "amb-prod-2" {
   ami             = "ami-0e2c8caa4b6378d8c"
   instance_type   = "t2.micro"
   security_groups = ["allow_http", "allow_ssh", "allow_egress"]
-
-  # Interpolação do script com o valor do PAT_TOKEN
-  user_data = templatefile("${path.module}/script.sh", {
-    PAT_TOKEN = var.github_secret
-  })
+  user_data       = file("script.sh")
 
   tags = {
     Name = "amb-prod"
