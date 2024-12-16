a9f40a5a6874a6c1da27f73cd38a1ac9dc1ed7e8
Gabriel Natan <gabriel--natan@live.com>
2024-12-16 00:14:48 -0300

    remove podman

--- terraform/script.sh
+++ terraform/script.sh
@@ -5,8 +5,7 @@ apt-get update
 apt-get install -y curl
 
 sudo apt install docker.io  -y
-sudo apt install podman-docker  -y
 
 echo "${var.github_secret}" | docker login ghcr.io -u gabrielnatan --password-stdin
 docker pull ghcr.io/gabrielnatan/front:main
-docker run -d ghcr.io/gabrielnatan/front:main
+docker run -d -p 80:80 ghcr.io/gabrielnatan/front:main
