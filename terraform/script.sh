86b0c8d586188480299a346bb0cc2cffd6565ae3
Gabriel Natan <gabriel--natan@live.com>
2024-12-16 00:39:10 -0300

    aqui

--- terraform/script.sh
+++ terraform/script.sh
@@ -2,13 +2,14 @@
 echo "Iniciando a configuração"
 
 # Atualiza o sistema e instala pacotes necessários
-apt-get update
-apt-get install -y curl
+sudo apt-get upgrade -y
+sudo apt-get update -y
+sudo apt-get install curl -Y
 sudo apt install docker.io -y
 
 # Faz login no Docker usando o token interpolado
-echo "${PAT_TOKEN}" | docker login ghcr.io -u user --password-stdin
+sudo echo "${PAT_TOKEN}" | docker login ghcr.io -u user --password-stdin
 
 # Baixa a imagem do contêiner e executa
-docker pull ghcr.io/user/front:main
-docker run -d -p 80:80 ghcr.io/user/front:main
+sudo docker pull ghcr.io/user/front:main
+sudo docker run -d -p 80:80 ghcr.io/user/front:main
