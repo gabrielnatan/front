29147df70c3413407d889f032d0b55544aee098e
Gabriel Natan <gabriel--natan@live.com>
2024-12-15 23:49:51 -0300

    sh

--- terraform/script.sh
+++ terraform/script.sh
@@ -4,8 +4,8 @@ echo "Iniciando a configuração"
 apt-get update
 apt-get install -y curl
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh ./get-docker.sh --dry-run
+sudo apt install docker.io  -y
+sudo apt install podman-docker  -y
 
 echo "${var.pat_token}" | docker login ghcr.io -u gabrielnatan --password-stdin
 docker pull ghcr.io/gabrielnatan/front:main
