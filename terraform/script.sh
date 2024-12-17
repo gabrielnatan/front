39c2de22ac8fd048a5f5de288ea6b3169bf807d2
Gabriel Natan <gabriel--natan@live.com>
2024-12-16 22:54:44 -0300

    add group

--- terraform/script.sh
+++ terraform/script.sh
@@ -2,13 +2,19 @@
 echo "Iniciando a configuração"
 
 # Atualiza o sistema e instala pacotes necessários
-sudo apt-get upgrade -y
 sudo apt-get update -y
-sudo apt-get install curl -Y
+sudo apt-get upgrade -y
+sudo apt-get install curl -y
 sudo apt install docker.io -y
 
+# Adiciona o usuário ao grupo docker (opcional, mas recomendado)
+sudo usermod -aG docker $USER
+
+# Reinicia o serviço Docker
+sudo systemctl restart docker
+
 # Faz login no Docker usando o token interpolado
-sudo echo "${PAT_TOKEN}" | docker login ghcr.io -u user --password-stdin
+echo "${PAT_TOKEN}" | sudo docker login ghcr.io -u user --password-stdin
 
 # Baixa a imagem do contêiner e executa
 sudo docker pull ghcr.io/user/front:main
