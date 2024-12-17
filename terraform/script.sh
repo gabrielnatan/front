a723f7372165844a1483163a5869d59dc09bb1f9
Gabriel Natan <gabriel--natan@live.com>
2024-12-16 23:12:18 -0300

    add user

--- terraform/script.sh
+++ terraform/script.sh
@@ -7,6 +7,8 @@ sudo apt-get upgrade -y
 sudo apt-get install curl -y
 sudo apt install docker.io -y
 
+sudo mkdir maneiro
+
 # Adiciona o usuário ao grupo docker (opcional, mas recomendado)
 sudo usermod -aG docker $USER
 
@@ -14,8 +16,11 @@ sudo usermod -aG docker $USER
 sudo systemctl restart docker
 
 # Faz login no Docker usando o token interpolado
-echo "${PAT_TOKEN}" | sudo docker login ghcr.io -u user --password-stdin
+echo "${PAT_TOKEN}" | sudo docker login ghcr.io -u gabrielnatan --password-stdin
 
 # Baixa a imagem do contêiner e executa
-sudo docker pull ghcr.io/user/front:main
-sudo docker run -d -p 80:80 ghcr.io/user/front:main
+sudo docker pull ghcr.io/gabrielnatan/front:main
+sudo docker run -d -p 80:80 ghcr.io/gabrielnatan/front:main
+
+sudo mkdir maneiro2
+sudo touch maneiro2/aqui.txt
